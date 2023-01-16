4e412dba41c0d09ccaeeaddbe9839641f81dae50
Igor-Zhelezniak-1 <90275833+Igor-Zhelezniak-1@users.noreply.github.com>
2023-01-16 09:42:17 -0500

    -

--- src/index.ts
+++ src/index.ts
@@ -9,211 +9,59 @@
 
 import Triangle from './Triangle'
 
+//input
 console.log('')
-
-const testTriangle1 = new Triangle(3, 4, 5)
-console.log('Created a 3 mm by 4 mm by 5 mm triangle.')
-console.log(
-  ' → Length of side1 of Triangle is: ' +
-    testTriangle1.getSide1().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side2 of Triangle is: ' +
-    testTriangle1.getSide2().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side2 of Triangle is: ' +
-    testTriangle1.getSide3().toFixed(4) +
-    ' mm'
-)
-console.log(' → Is the triangle valid: ' + testTriangle1.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + testTriangle1.getSemiPerimeter().toFixed(4) + ' mm'
-)
-console.log(' → Area: ' + testTriangle1.getArea().toFixed(4) + ' mm²')
-console.log(' → Type: ' + testTriangle1.getTriangleType())
-console.log(' → Angle 1: ' + testTriangle1.getAngle(1).toFixed(4) + ' rad')
-console.log(' → Angle 2: ' + testTriangle1.getAngle(2).toFixed(4) + ' rad')
-console.log(' → Angle 3: ' + testTriangle1.getAngle(3).toFixed(4) + ' rad')
-console.log(' → Height 1: ' + testTriangle1.getHeight(1).toFixed(4) + ' mm')
-console.log(' → Height 2: ' + testTriangle1.getHeight(2).toFixed(4) + ' mm')
-console.log(' → Height 3: ' + testTriangle1.getHeight(3).toFixed(4) + ' mm')
-console.log(
-  ' → Inner circle radius: ' +
-    testTriangle1.getinnerCircleRadius().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Circumcircle radius: ' +
-    testTriangle1.getcircumRadius().toFixed(4) +
-    ' mm'
-)
-console.log('')
-
-const testTriangle2 = new Triangle(3, 3, 3)
-console.log('Created a 3 mm by 3 mm by 3 mm triangle.')
-console.log(
-  ' → Length of side1 of Triangle is: ' +
-    testTriangle2.getSide1().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side2 of Triangle is: ' +
-    testTriangle2.getSide2().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side3 of Triangle is: ' +
-    testTriangle2.getSide3().toFixed(4) +
-    ' mm'
-)
-console.log(' → Is the triangle valid: ' + testTriangle2.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + testTriangle2.getSemiPerimeter().toFixed(4) + ' mm'
-)
-console.log(' → Area: ' + testTriangle2.getArea().toFixed(4) + ' mm²')
-console.log(' → Type: ' + testTriangle2.getTriangleType())
-console.log(' → Angle 1: ' + testTriangle2.getAngle(1).toFixed(4) + ' rad')
-console.log(' → Angle 2: ' + testTriangle2.getAngle(2).toFixed(4) + ' rad')
-console.log(' → Angle 3: ' + testTriangle2.getAngle(3).toFixed(4) + ' rad')
-console.log(' → Height 1: ' + testTriangle2.getHeight(1).toFixed(4) + ' mm')
-console.log(' → Height 2: ' + testTriangle2.getHeight(2).toFixed(4) + ' mm')
-console.log(' → Height 3: ' + testTriangle2.getHeight(3).toFixed(4) + ' mm')
-console.log(
-  ' → Inner circle radius: ' +
-    testTriangle2.getinnerCircleRadius().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Circumcircle radius: ' +
-    testTriangle2.getcircumRadius().toFixed(4) +
-    ' mm'
-)
-console.log('')
-
-const testTriangle3 = new Triangle(3, 4, 4)
-console.log('Created a 3 mm by 4 mm by 4 mm triangle.')
-console.log(
-  ' → Length of side1 of Triangle is: ' +
-    testTriangle3.getSide1().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side2 of Triangle is: ' +
-    testTriangle3.getSide2().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side3 of Triangle is: ' +
-    testTriangle3.getSide3().toFixed(4) +
-    ' mm'
-)
-console.log(' → Is the triangle valid: ' + testTriangle3.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + testTriangle3.getSemiPerimeter().toFixed(4) + ' mm'
-)
-console.log(' → Area: ' + testTriangle3.getArea().toFixed(4) + ' mm²')
-console.log(' → Type: ' + testTriangle3.getTriangleType())
-console.log(' → Angle 1: ' + testTriangle3.getAngle(1).toFixed(4) + ' rad')
-console.log(' → Angle 2: ' + testTriangle3.getAngle(2).toFixed(4) + ' rad')
-console.log(' → Angle 3: ' + testTriangle3.getAngle(3).toFixed(4) + ' rad')
-console.log(' → Height 1: ' + testTriangle3.getHeight(1).toFixed(4) + ' mm')
-console.log(' → Height 2: ' + testTriangle3.getHeight(2).toFixed(4) + ' mm')
-console.log(' → Height 3: ' + testTriangle3.getHeight(3).toFixed(4) + ' mm')
-console.log(
-  ' → Inner circle radius: ' +
-    testTriangle3.getinnerCircleRadius().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Circumcircle radius: ' +
-    testTriangle3.getcircumRadius().toFixed(4) +
-    ' mm'
-)
-console.log('')
-
-const testTriangle4 = new Triangle(2, 3, 4)
-console.log('Created a 2 mm by 3 mm by 4 mm triangle.')
-console.log(
-  ' → Length of side1 of Triangle is: ' +
-    testTriangle4.getSide1().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side2 of Triangle is: ' +
-    testTriangle4.getSide2().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side3 of Triangle is: ' +
-    testTriangle4.getSide3().toFixed(4) +
-    ' mm'
-)
-console.log(' → Is the triangle valid: ' + testTriangle4.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + testTriangle4.getSemiPerimeter().toFixed(4) + ' mm'
-)
-console.log(' → Area: ' + testTriangle4.getArea().toFixed(4) + ' mm²')
-console.log(' → Type: ' + testTriangle4.getTriangleType())
-console.log(' → Angle 1: ' + testTriangle4.getAngle(1).toFixed(4) + ' rad')
-console.log(' → Angle 2: ' + testTriangle4.getAngle(2).toFixed(4) + ' rad')
-console.log(' → Angle 3: ' + testTriangle4.getAngle(3).toFixed(4) + ' rad')
-console.log(' → Height 1: ' + testTriangle4.getHeight(1).toFixed(4) + ' mm')
-console.log(' → Height 2: ' + testTriangle4.getHeight(2).toFixed(4) + ' mm')
-console.log(' → Height 3: ' + testTriangle4.getHeight(3).toFixed(4) + ' mm')
-console.log(
-  ' → Inner circle radius: ' +
-    testTriangle4.getinnerCircleRadius().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Circumcircle radius: ' +
-    testTriangle4.getcircumRadius().toFixed(4) +
-    ' mm'
-)
-console.log('')
-
-const testTriangle5 = new Triangle(23, 4, 6)
-console.log('Created a 23 mm by 3 mm by 4 mm triangle.')
-console.log(
-  ' → Length of side1 of Triangle is: ' +
-    testTriangle5.getSide1().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side2 of Triangle is: ' +
-    testTriangle5.getSide2().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Length of side3 of Triangle is: ' +
-    testTriangle5.getSide3().toFixed(4) +
-    ' mm'
-)
-console.log(' → Is the triangle valid: ' + testTriangle5.isValid().toString())
-console.log(
-  ' → Semiperimeter: ' + testTriangle5.getSemiPerimeter().toFixed(4) + ' mm'
-)
-console.log(' → Area: ' + testTriangle5.getArea().toFixed(4) + ' mm²')
-console.log(' → Type: ' + testTriangle5.getTriangleType())
-console.log(' → Angle 1: ' + testTriangle5.getAngle(1).toFixed(4) + ' rad')
-console.log(' → Angle 2: ' + testTriangle5.getAngle(2).toFixed(4) + ' rad')
-console.log(' → Angle 3: ' + testTriangle5.getAngle(3).toFixed(4) + ' rad')
-console.log(' → Height 1: ' + testTriangle5.getHeight(1).toFixed(4) + ' mm')
-console.log(' → Height 2: ' + testTriangle5.getHeight(2).toFixed(4) + ' mm')
-console.log(' → Height 3: ' + testTriangle5.getHeight(3).toFixed(4) + ' mm')
-console.log(
-  ' → Inner circle radius: ' +
-    testTriangle5.getinnerCircleRadius().toFixed(4) +
-    ' mm'
-)
-console.log(
-  ' → Circumcircle radius: ' +
-    testTriangle5.getcircumRadius().toFixed(4) +
-    ' mm'
-)
+const sside1 = prompt('Enter side1: ')
+const sside2 = prompt('Enter side2: ')
+const sside3 = prompt('Enter side3: ')
+try {
+  const side1 = parseInt(sside1)
+  const side2 = parseInt(sside2)
+  const side3 = parseInt(sside3)
+  if (isNaN(side1)||isNaN(side2)||isNaN(side3)) {
+    console.log('Invalid Input')
+  } else {
+    const testTriangle1 = new Triangle(side1, side2, side3)
+    console.log(`Created a ${side1} mm by ${side2} mm by ${side3} mm triangle.`)
+    console.log(
+      ' → Length of side1 of Triangle is: ' +
+        testTriangle1.getSide1().toFixed(4) +
+        ' mm'
+    )
+    console.log(
+      ' → Length of side2 of Triangle is: ' +
+        testTriangle1.getSide2().toFixed(4) +
+        ' mm'
+    )
+    console.log(
+      ' → Length of side2 of Triangle is: ' +
+        testTriangle1.getSide3().toFixed(4) +
+        ' mm'
+    )
+    console.log(' → Is the triangle valid: ' + testTriangle1.isValid().toString())
+    console.log(
+      ' → Semiperimeter: ' + testTriangle1.getSemiPerimeter().toFixed(4) + ' mm'
+    )
+    console.log(' → Area: ' + testTriangle1.getArea().toFixed(4) + ' mm²')
+    console.log(' → Type: ' + testTriangle1.getTriangleType())
+    console.log(' → Angle 1: ' + testTriangle1.getAngle(1).toFixed(4) + ' rad')
+    console.log(' → Angle 2: ' + testTriangle1.getAngle(2).toFixed(4) + ' rad')
+    console.log(' → Angle 3: ' + testTriangle1.getAngle(3).toFixed(4) + ' rad')
+    console.log(' → Height 1: ' + testTriangle1.getHeight(1).toFixed(4) + ' mm')
+    console.log(' → Height 2: ' + testTriangle1.getHeight(2).toFixed(4) + ' mm')
+    console.log(' → Height 3: ' + testTriangle1.getHeight(3).toFixed(4) + ' mm')
+    console.log(
+      ' → Inner circle radius: ' +
+        testTriangle1.getinnerCircleRadius().toFixed(4) +
+        ' mm'
+    )
+    console.log(
+      ' → Circumcircle radius: ' +
+        testTriangle1.getcircumRadius().toFixed(4) +
+        ' mm'
+    )
+  }
+}
 console.log('')
 
 console.log('\nDone.')
